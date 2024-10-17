59c218c81e4435076e4a6196c9eb4e20fedd0521
fatima <fatima43tair@gmail.com>
2024-10-17 12:52:57 +0300

    fixed doa

--- app/src/main/java/com/example/tp4/model/post/room/PostDao.kt
+++ app/src/main/java/com/example/tp4/model/post/room/PostDao.kt
@@ -8,23 +8,23 @@ import com.example.tp4.model.post.Post
 interface PostDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertPost(post: Post) // Utilisez suspend pour les opérations longues
+    fun insertPost(post: Post) // Utilisez suspend pour les opérations longues
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertPosts(posts: List<Post>) // Utilisez suspend pour les opérations longues
+    fun insertPosts(posts: List<Post>) // Utilisez suspend pour les opérations longues
 
     @Query("SELECT * FROM posts")
     fun getAllPosts(): LiveData<List<Post>> // LiveData pour observer les changements
 
     @Query("SELECT * FROM posts WHERE id = :postId")
-    suspend fun getPostById(postId: Int): Post? // Retournez Post? pour les résultats potentiellement null
+    fun getPostById(postId: Int): Post? // Retournez Post? pour les résultats potentiellement null
 
     @Update
-    suspend fun updatePost(post: Post) // Utilisez suspend pour les opérations longues
+    fun updatePost(post: Post) // Utilisez suspend pour les opérations longues
 
     @Delete
-    suspend fun deletePost(post: Post) // Utilisez suspend pour les opérations longues
+   fun deletePost(post: Post) // Utilisez suspend pour les opérations longues
 
     @Query("DELETE FROM posts")
-    suspend fun deleteAllPosts() // Utilisez suspend pour les opérations longues
+    fun deleteAllPosts() // Utilisez suspend pour les opérations longues
 }
